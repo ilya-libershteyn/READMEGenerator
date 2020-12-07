e41ac6aa422558aa5e6ebf0553f263820432eb2a
Ilya Libershteyn <ilya.v.libershteyn@gmail.com>
2020-12-06 21:24:11 -0800

    defined questions array and generated markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,22 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title}  
+## Description  
+${data.descrtption}  
+## Table of Contents  
+${data.contents}  
+## Installation  
+${data.install}  
+## Usage  
+${data.usage}  
+## License  
+${data.license}  
+## Contributing  
+${data.contributing}  
+## Tests  
+${data.tests}  
+## Questions  
+${data.name}
 
 `;
 }
